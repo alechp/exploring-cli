da0331b90d6261687516b0c07cc642666dc43518
alechp <me@alechp.com>
2018-02-10 07:33:37 -0800

    Added inquirer and commander egs.

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-/**
- * @Author: Alec Hale-Pletka <alechp>
- * @Date:   2018-01-24T13:57:15-08:00
- * @Email:  alec@bubblegum.academy
- * @Last modified by:   alechp
- * @Last modified time: 2018-01-24T14:05:46-08:00
- */
-console.log("hi");
+const { inq } = require("./src/inquirer.js");
+const { cmd } = require("./src/commander.js");
+
+//Toggle between options below. Only one function at a time
+
+inq();
+// cmd();
---
+++ src/inquirer.js
@@ -0,0 +1,54 @@
+const inquirer = require("inquirer");
+const log = console.log;
+
+function inq() {
+  let stdChoices = ["A", "B", "C", "D", "E"];
+  let prompt = inquirer.createPromptModule();
+  let questions = [
+    {
+      name: "A",
+      type: "list",
+      message: "Select one option & press Enter",
+      choices: stdChoices
+    },
+    {
+      name: "B",
+      type: "rawlist",
+      message: "Select one option & press Enter",
+      choices: stdChoices
+    },
+    {
+      name: "D",
+      type: "checkbox",
+      message: "Select one or more options & press Enter",
+      choices: stdChoices
+    },
+    {
+      name: "E",
+      type: "confirm",
+      message: "Yes or no?"
+    },
+    {
+      name: "F",
+      type: "input",
+      message: "Input?"
+    },
+    {
+      name: "G",
+      type: "password",
+      message: "Password?"
+    },
+    {
+      name: "H",
+      type: "editor",
+      message: "Editor"
+    }
+  ];
+  prompt(questions)
+    .then(answers => log(`answers: ${JSON.stringify(answers, null, 2)}`))
+    .catch(err => err);
+}
+
+module.exports = {
+  inq
+};
